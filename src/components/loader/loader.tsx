515c2923bad9ea4b25fd9bfc2f27035a79ed7ecd
Dimitri Dumont <contact@dimitri-dumont.fr>
2022-01-04 14:02:24 +0100

    feature: create loader component

---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactLoaderSpinner from "react-loader-spinner"
+
+export const Loader = () => {
+	return (
+		<div>
+			<ReactLoaderSpinner type={"Circles"} />
+		</div>
+	)
+}
