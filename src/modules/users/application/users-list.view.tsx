d0efea1e04b996cf6144a8307adf48c62366dc94
Dimitri Dumont <contact@dimitri-dumont.fr>
2022-01-04 14:46:02 +0100

    feature: stylize users list

--- src/modules/users/application/users-list.view.tsx
+++ src/modules/users/application/users-list.view.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { User } from "@/types/user"
 import { FiltersContainer } from "@/modules/users/application/components/filters/filters.container"
 import { Link } from "@/components/link/link"
+import styles from "./users-list.module.scss"
 
 interface Props {
 	usersList: User[]
@@ -13,32 +14,51 @@ export const UsersListView = ({ usersList, filterUsers }: Props) => {
 		<>
 			<FiltersContainer filterUsers={filterUsers} />
 
-			<div>
-				{usersList.map((user: User) => (
-					<div key={user.id}>
-						<div>
-							<div>{user.name}</div>
-							<div>{user.email}</div>
-						</div>
-						<div>
-							<span>{user.address.street}</span>{" "}
-							<span>{user.address.city}</span>
-						</div>
-						<div>{user.phone}</div>
-						<div>
-							<Link
-								href={user.website}
-								target="_blank"
-								rel="noopener, noreferrer"
-							>
-								{user.website}
-							</Link>
-						</div>
-						<div>
-							<Link href={`/users/${user.id}`}>List posts</Link>
-						</div>
-					</div>
-				))}
+			<div className={styles.container}>
+				<table className={styles.table}>
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Address</th>
+							<th>Phone</th>
+							<th>Website</th>
+							<th>Edit</th>
+						</tr>
+					</thead>
+					<tbody>
+						{usersList.map((user: User) => (
+							<tr key={user.id}>
+								<td>
+									<div className={styles.userName}>
+										{user.name}
+									</div>
+									<div>{user.email}</div>
+								</td>
+								<td>
+									<span>{user.address.street}</span>{" "}
+									<span className={styles.userCity}>
+										{user.address.city}
+									</span>
+								</td>
+								<td>{user.phone}</td>
+								<td>
+									<Link
+										href={user.website}
+										target="_blank"
+										rel="noopener, noreferrer"
+									>
+										{user.website}
+									</Link>
+								</td>
+								<td>
+									<Link href={`/users/${user.id}`}>
+										List posts
+									</Link>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
 			</div>
 		</>
 	)
