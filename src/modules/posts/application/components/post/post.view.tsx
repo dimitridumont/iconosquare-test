1b92107b4b1c9a7573b39e5b1afd6ec374460323
Dimitri Dumont <contact@dimitri-dumont.fr>
2022-01-04 15:10:04 +0100

    feature: stylize user post item

--- src/modules/posts/application/components/post/post.view.tsx
+++ src/modules/posts/application/components/post/post.view.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Post } from "@/types/post"
 import { Modal } from "@/components/modal/modal"
 import { CommentsContainer } from "@/modules/comments/application/comments.container"
+import styles from "./post.module.scss"
 
 interface Props {
 	post: Post
@@ -17,24 +18,24 @@ export const PostView = ({
 	closeCommentsModal,
 }: Props) => {
 	return (
-		<>
+		<div className={styles.container}>
 			<div onClick={openCommentsModal}>
-				<h3>{post.title}</h3>
+				<h3 className={styles.title}>{post.title}</h3>
 
-				<div>{post.body}</div>
+				<div className={styles.body}>{post.body}</div>
 			</div>
 
 			<Modal show={isCommentsModalVisible} onHide={closeCommentsModal}>
 				<>
 					<div>
-						<h3>{post.title}</h3>
+						<h3 className={styles.title}>{post.title}</h3>
 
-						<div>{post.body}</div>
+						<div className={styles.body}>{post.body}</div>
 					</div>
 
 					<CommentsContainer postID={post.id} />
 				</>
 			</Modal>
-		</>
+		</div>
 	)
 }
